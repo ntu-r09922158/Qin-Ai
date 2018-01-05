b49e4989a9042c0f5905fbbb5619f6d7031397b0
user-PC\user <d0527783@fcu.edu.tw>
2018-01-05 23:18:33 +0800

    add bootstrap feature

---
+++ jQuery.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+
+			$(document).ready(function(){
+				
+				$("#1").mouseover(function(){ 
+				$("#1").animate({fontSize: '2em'}, "slow");
+				});
+				$("#1").mouseleave(function(){ 
+				$("#1").animate({fontSize: '1em'}, "slow");
+				});
+				
+				$("#2").mouseover(function(){ 
+				$("#2").animate({fontSize: '2em'}, "slow");
+				});
+				$("#2").mouseleave(function(){ 
+				$("#2").animate({fontSize: '1em'}, "slow");
+				});
+				
+				$("#3").mouseover(function(){ 
+				$("#3").animate({fontSize: '2em'}, "slow");
+				});
+				$("#3").mouseleave(function(){ 
+				$("#3").animate({fontSize: '1em'}, "slow");
+				});
+				
+				$("#4").mouseover(function(){ 
+				$("#4").animate({fontSize: '2em'}, "slow");
+				});
+				$("#4").mouseleave(function(){ 
+				$("#4").animate({fontSize: '1em'}, "slow");
+				});
+				
+				$("#5").mouseover(function(){ 
+				$("#5").animate({fontSize: '2em'}, "slow");
+				});
+				$("#5").mouseleave(function(){ 
+				$("#5").animate({fontSize: '1em'}, "slow");
+				});
+			});
---
+++ javascript.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var service = ["定期上載仁愛村的最新資訊", "物資收集與輸送", "募款", "為村民尋求資助", "溝通橋樑"];
+
+/*for(var i=0;i<service.length;i++){
+	
+	document.getElementById("service").innerHTML = "<li>"+service[i]+"</li>";
+
+}*/
+//document.getElementById("service").innerHTML = service;
+document.getElementById("service").innerHTML = "<div class=\"row\"><li class=\"col-sm-12\">"+service[0]+
+																"</li></div></div><div class=\"row\"><li class=\"col-sm-12\">"+service[1]+
+																"</li></div></div><div class=\"row\"><li class=\"col-sm-12\">"+service[2]+
+																"</li></div></div><div class=\"row\"><li class=\"col-sm-12\">"+service[3]+
+																"</li></div></div><div class=\"row\"><li class=\"col-sm-12\">"+service[4]+"</li>";
